9bf834eabeb8e6ca60fc7b406d5c84ffacd1df6a
Edmund Pfeil <edmundpf@buffalo.edu>
2019-06-03 07:31:07 -0400

    Completed printer class.

--- unit_tests.py
+++ unit_tests.py
@@ -1,1 +1,7 @@
-from utils import function_tests
+import unittest
+import doctest
+
+testSuite = unittest.TestSuite()
+# for mod in dict_functions, list_functions, string_functions, float_functions:
+# 	testSuite.addTest(doctest.DocTestSuite(mod))
+unittest.TextTestRunner().run(testSuite)
