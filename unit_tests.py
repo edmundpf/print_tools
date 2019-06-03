1e850f8e9081329be6b1646d9d09938bce2a5a5f
Edmund Pfeil <edmundpf@buffalo.edu>
2019-06-03 07:32:52 -0400

    Removed test file. Edited travis yml

--- unit_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import doctest
-
-testSuite = unittest.TestSuite()
-# for mod in dict_functions, list_functions, string_functions, float_functions:
-# 	testSuite.addTest(doctest.DocTestSuite(mod))
-unittest.TextTestRunner().run(testSuite)
